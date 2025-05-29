65a2e4c832ce0c3d4478f59f2e37b2407130d4b8
MedBrk <150671350+med-ctrl@users.noreply.github.com>
2025-05-29 21:15:53 +0100

    chore: Build Responsive Listing Page with Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
